b7927f1232ee38b567484717623cfce636b296b7
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-10-19 06:36:24 +0300

    Update run.ps1

--- Disable IPv6/run.ps1
+++ Disable IPv6/run.ps1
@@ -1,6 +1,6 @@
 $registryPath = "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters"
 try {
-  Write-Host "[i] Optimizing $path > $Value" -ForegroundColor Cyan
+  Write-Host "[i] Optimizing $path > 255" -ForegroundColor Cyan
   Set-ItemProperty -Path $registryPath -Name "DisabledComponents" -Value 255 -Type DWord
   Disable-NetAdapterBinding -Name \"*\" -ComponentID ms_tcpip6
 }

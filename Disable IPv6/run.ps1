3df8f091086141820698800fe657cbb3b746058e
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-10-19 05:58:12 +0300

    Create run.ps1

---
+++ Disable IPv6/run.ps1
@@ -0,0 +1,9 @@
+$registryPath = "HKLM:\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters"
+try {
+  Write-Host "[i] Optimizing $path > $Value" -ForegroundColor Cyan
+  Set-ItemProperty -Path $registryPath -Name "DisabledComponents" -Value 255 -Type DWord
+  Disable-NetAdapterBinding -Name \"*\" -ComponentID ms_tcpip6
+}
+catch {
+    Write-Host "[!] $($_.Exception.Message)" -ForegroundColor Red
+}

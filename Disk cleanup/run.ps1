d5a35151a586f12b8258e351aa29495e5dd74083
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-11-20 04:24:46 +0200

    Update run.ps1

--- Disk cleanup/run.ps1
+++ Disk cleanup/run.ps1
@@ -9,7 +9,7 @@ icacls C:\Windows\SoftwareDistribution /grant administrators:F /t
 Remove-Item -Path "C:\Windows\SoftwareDistribution\*" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "C:\Windows\SoftwareDistribution\DeliveryOptimization\*" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "C:\Windows\WinSxS\Backup\*" -Recurse -Force -ErrorAction SilentlyContinue
-cleanmgr.exe /d C: /VERYLOWDISK /sagerun:1
+cleanmgr.exe /d C: /VERYLOWDISK
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 wevtutil.exe clear-log "Microsoft-Windows-WindowsUpdateClient/Operational"
 Remove-Item -Path "C:\Windows\Installer\$PatchCache$\*" -Recurse -Force -ErrorAction SilentlyContinue

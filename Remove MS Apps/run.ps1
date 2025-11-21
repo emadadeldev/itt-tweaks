6977b3d8419250eae4545793d0a2345aaa4fc328
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-11-21 05:30:57 +0200

    Update run.ps1

--- Remove MS Apps/run.ps1
+++ Remove MS Apps/run.ps1
@@ -84,7 +84,7 @@ $AppxPackages = @(
 foreach ($name in $AppxPackages) {
     try {
         Write-Host "[i] Attempting to remove $name" -ForegroundColor Cyan
-        Get-AppxPackage -Name $name | Remove-AppxPackage  -ErrorAction SilentlyContinue
+        Get-AppxPackage -Name $name | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
         Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
     } catch {
         Write-Host "[x] PLEASE USE (WINDOWS POWERSHELL) NOT (TERMINAL POWERSHELL 7) TO UNINSTALL"
